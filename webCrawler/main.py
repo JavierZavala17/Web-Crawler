9855e1970de0ba321cc5e71d8764f0acacc22dbf
JavierZavala17 <javier_z_t@hotmail.com>
2026-09-24 19:28:15 -0600

    Main

--- webCrawler/main.py
+++ webCrawler/main.py
@@ -1,5 +1,15 @@
+import sys
+
 def main():
-    print("Hello from webcrawler!")
+    if len(sys.argv) < 2:
+        print(f"no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print(f"too many arguments provided")
+        sys.exit(1)
+    else:
+        base_url = sys.argv[1]
+        print(f"starting crawl of: {base_url}")
 
 
 if __name__ == "__main__":
